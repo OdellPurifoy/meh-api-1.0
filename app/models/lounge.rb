6f303d29ec65a47c6bf629e9cca011ebd09bdec5
Odell Purifoy <opurifoy@springhealth.com>
2023-03-17 18:38:56 -0400

    Added model validation to lounge

--- app/models/lounge.rb
+++ app/models/lounge.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 class Lounge < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :name, :phone
+  validates :description, length: { maximum: 1000, too_long: '%<count>s character is the maximum allowed' }
 end
