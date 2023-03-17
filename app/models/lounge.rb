23016215697b7d5a9f0df5cd482e790329542af2
Odell Purifoy <opurifoy@springhealth.com>
2023-03-17 19:00:25 -0400

    Annotated models specs and factories

--- app/models/lounge.rb
+++ app/models/lounge.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: lounges
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  phone       :string           not null
+#  email       :string
+#  description :text
+#  user_id     :bigint           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Lounge < ApplicationRecord
   belongs_to :user
 
--- spec/models/lounge_spec.rb
+++ spec/models/lounge_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: lounges
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  phone       :string           not null
+#  email       :string
+#  description :text
+#  user_id     :bigint           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Lounge, type: :model do
