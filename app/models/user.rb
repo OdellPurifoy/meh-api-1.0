6599d31bb848d0c8d05a0f9653704db76966e56c
Odell Purifoy <opurifoy@springhealth.com>
2023-03-17 19:15:24 -0400

    Added has many relationship to user model for lounges

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+  has_many :lounges
 end
